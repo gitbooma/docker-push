72624e53143423313fc660c2bc8d2187f1bf2f2b
gitbooma <96261187+gitbooma@users.noreply.github.com>
2022-09-22 13:44:59 +0200

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -4,11 +4,15 @@ OVERRIDE="$2"
 DOC_USER="boomadevops"
 
 echo "VERSION=$VERSION"
+echo "doc_user=$DOC_USER"
 echo "OVERRIDE=$OVERRIDE"
+
 
 #docker login ghcr.io -u ${GITHUB_REF} -p ${REPO_TOKEN}
 
 VERSION=$VERSION docker-compose -f $OVERRIDE up --no-start --remove-orphans
+echo "compose done"
+echo "VER=$VERSION"
 IMAGES=$(docker inspect --format='{{.Image}}' $(docker ps -aq))
 
 echo "IMAGES: $IMAGES"
@@ -17,7 +21,7 @@ for IMAGE in $IMAGES; do
     
    # NAME=$(basename ${GITHUB_REPOSITORY}).$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
     NAME=$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
-    echo $NAME
+    echo "NAME=$NAME"
     #TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
     TAG="${DOC_USER}/$NAME:$VERSION"
     echo $TAG
