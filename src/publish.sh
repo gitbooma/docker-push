80b719d144530e215b67c6617e09a548f6bb81c9
gitbooma <devibooma@gmail.com>
2022-09-23 17:51:40 +0200

    only build and start

--- src/publish.sh
+++ src/publish.sh
@@ -3,7 +3,7 @@ echo "in publish"
 DOCKER_COMPOSE_FILE="$1"
 TAG="$2"
 
-TAG=$TAG docker-compose -f $DOCKER_COMPOSE_FILE up --remove-orphans
+TAG=$TAG docker-compose -f $DOCKER_COMPOSE_FILE build --remove-orphans
 echo "compose done"
 IMAGES_OUTPUT=$(docker ps --format '{{.Image}})
 echo "docker ps names"
@@ -11,3 +11,4 @@ echo $IMAGES_OUTPUT
 
     #TAG=docker ps --format '{{.Image}}
     docker compose push
+    docker compose start
