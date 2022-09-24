3daa9045d738db5d0bb03b20a71178435afb75e0
gitbooma <devibooma@gmail.com>
2022-09-24 22:07:08 +0200

    test

--- src/publish.sh
+++ src/publish.sh
@@ -1,13 +1,24 @@
 
 echo "in publish"
-DOCKER_COMPOSE_FILE="$1"
-echo "1 $TAG"
+#DOCKER_COMPOSE_FILE="$1"
+echo "10000000000 $TAG"
+echo $THEME
 
-TAG=$TAG docker-compose -f $DOCKER_COMPOSE_FILE up --no-start --remove-orphans
+if [[ -n "$THEME"]
+then
+TAG="$THEME:$TAG"
+echo "combine"
+echo $TAG
+fi
+
+echo "outside"
+echo $TAG
+
+#TAG=$TAG docker-compose -f $DOCKER_COMPOSE_FILE up --no-start --remove-orphans
 echo "compose done"
-IMAGES_OUTPUT=$(docker images)
+#IMAGES_OUTPUT=$(docker images)
 echo "docker ps names"
-echo $IMAGES_OUTPUT
+#echo $IMAGES_OUTPUT
 
 #TAG=docker ps --format '{{.Image}}
-docker compose push
+#docker compose push
