f24fa44a36f673455b1b10d48baa8a618450cda6
gitbooma <devibooma@gmail.com>
2022-09-25 10:58:28 +0200

    tries

--- src/publish.sh
+++ src/publish.sh
@@ -1,15 +1,15 @@
 
 echo "in publish"
-#DOCKER_COMPOSE_FILE="$1"
+DOCKER_COMPOSE_FILE="$1"
 echo "10000000000 $TAG"
 echo "Theme $THEME"
 
 
-if [ -n "${THEME}" ]; then
-TAG="$THEME-$TAG"
-echo "combine"
-echo $TAG
-fi
+#if [ -n "${THEME}" ]; then
+#TAG="$THEME-$TAG"
+#echo "combine"
+#echo $TAG
+#fi
 
 echo "outside"
 echo $TAG
